ad36b81128c2b32e71fb03d9f1f39a1cbfd20b6f
agrha <agrhakartz@gmail.com>
2018-04-16 14:54:57 +0700

    repair typo

--- server/models/blog.model.js
+++ server/models/blog.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
-const blogSchema = Schema({
+const blogSchema = new Schema({
   title : {
     type: String,
     default: 'Untitled'
