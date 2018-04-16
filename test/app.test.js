5babc089f76ded37cce183bb3adc5007532a0869
barestu <fheartnet@gmail.com>
2018-04-17 06:55:40 +0700

    Add test assert with expect method

--- test/app.test.js
+++ test/app.test.js
@@ -6,24 +6,81 @@ const app = require('../app')
 chai.use(chaiHttp)
 
 describe('test blog api', () => {
+  describe('show all posted article', () => {
+    it('should show all posted article', (done) => {
+      chai.request(app)
+          .get('/api/post/find')
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            expect(res.body).to.ownProperty('message').to.equal('Show all Post')
+            expect(res.body).to.ownProperty('data').to.be.an('array')
+            done()
+          })
+    })
+  })
+
   describe('post new article', () => {
-    it('show posted article', (done) => {
+    it('should show posted article', (done) => {
       chai.request(app)
           .post('/api/post/add')
           .type('form')
           .send({
-            _method: 'post',
             post_title: 'Post Title',
             post_content: 'Lorem ipsum dolor sit amet'
           })
-          .end( (err, res) => {
+          .end((err, res) => {
             expect(err).to.be.null
             expect(res).to.have.status(201)
-            expect(res.body.data).to.have.property('post_title')
-            expect(res.body.data).to.have.property('post_content')
+            expect(res.body).to.ownProperty('message').to.equal('Post new article success')
+            expect(res.body).to.ownProperty('data').to.be.a('object')
+            expect(res.body.data).to.ownProperty('post_title').to.be.a('string')
+            expect(res.body.data).to.ownProperty('post_content').to.be.a('string')
             done()
           })
     })
+  })
 
+  describe('update article by ID', () => {
+    it('should show updated article', (done) => {
+      chai.request(app)
+          .get('/api/post/find')
+          .end((err, res) => {
+            chai.request(app)
+                .put(`/api/post/update/${res.body.data[4]._id}`)
+                .type('form')
+                .send({
+                  post_title: 'New Post Title',
+                  post_content: 'New lorem ipsum dolor sit amet'
+                })
+                .end((err, res) => {
+                  expect(err).to.be.null
+                  expect(res).to.have.status(200)
+                  expect(res.body).to.ownProperty('message').to.equal('Update article success')
+                  expect(res.body).to.ownProperty('data').to.be.a('object')
+                  expect(res.body.data).to.ownProperty('post_title').to.be.a('string')
+                  expect(res.body.data).to.ownProperty('post_content').to.be.a('string')
+                  done()
+                })
+          })
+    })
+  })
+
+  describe('delete article by ID', () => {
+    it('should delete one article by ID', (done) => {
+      chai.request(app)
+          .get('/api/post/find')
+          .end((err, res) => {
+            chai.request(app)
+                .delete(`/api/post/delete/${res.body.data[4]._id}`)
+                .end((err, res) => {
+                  expect(err).to.be.null
+                  expect(res).to.have.status(200)
+                  expect(res.body).to.ownProperty('message').to.equal('Delete article success')
+                  expect(res.body).to.ownProperty('data').to.be.a('object')
+                  done()
+                })
+          })
+    })
   })
 })
