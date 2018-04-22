156271104d4349ba3fcdf7420fe592b926949303
barestu <fheartnet@gmail.com>
2018-04-22 09:51:19 +0700

    change db server

--- server/app.js
+++ server/app.js
@@ -6,11 +6,14 @@ const cors = require('cors')
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
+const db_user = process.env.DB_USER
+const db_pass = process.env.DB_PASS
 
 const postRoute = require('./routers/posts')
 const userRoute = require('./routers/users')
 
-mongoose.connect(`mongodb://localhost/blog`)
+// mongoose.connect(`mongodb://localhost/blog`)
+mongoose.connect(`mongodb://${db_user}:${db_pass}@ds247759.mlab.com:47759/barestu-blog`)
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
@@ -25,8 +28,8 @@ app.use(morgan('dev'))
 app.use('/api/post', postRoute)
 app.use('/api/user', userRoute)
 
-app.listen(port, function() {
-  console.log('Listening on port', port)
-})
+// app.listen(port, function() {
+//   console.log('Listening on port', port)
+// })
 
 module.exports = app
