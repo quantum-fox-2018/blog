282e90c0cac1924282d838bad9d1b805a54f0017
Zena <zena.vilenia@gmail.com>
2018-04-25 10:37:37 +0700

    edit admin

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {findAll, findById, add, addComment, update, remove} = require('../controllers/post.controller')
+const {findAll, findById, add, addComment, update,updateNoImage, remove} = require('../controllers/post.controller')
 const multer = require('multer')
 const uploadMidleware = require('../middleware/upload')
 const {auth} = require('../middleware/auth')
@@ -16,7 +16,8 @@ router
     .get('/:id', findById)
     .post('/', uploaderMem.single('image'),uploadMidleware.upload, add)
     .post('/addcomment', auth, addComment)
-    .put('/:id', uploaderMem.single('image'),uploadMidleware.upload, update)
+    .put('/', uploaderMem.single('image'),uploadMidleware.upload, update)
+    .put('/noImage', updateNoImage)
     .delete('/:id', remove)
 
 module.exports = router
